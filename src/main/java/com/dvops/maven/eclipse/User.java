227b27abcec0463aa9d2dfef54361a568f5bdd14
hongchaoying <2100910E@student.tp.edu.sg>
2022-12-05 21:18:00 +0800

    initial user model

---
+++ src/main/java/com/dvops/maven/eclipse/User.java
@@ -0,0 +1,79 @@
+package com.dvops.maven.eclipse;
+
+public class User {
+	protected String name;
+	/**
+	 * @param name
+	 * @param password
+	 * @param email
+	 * @param language
+	 */
+	public User(String name, String password, String email, String language) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.language = language;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	/**
+	 * @return the language
+	 */
+	public String getLanguage() {
+		return language;
+	}
+	
+	/**
+	 * @param language the language to set
+	 */
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+	protected String password;
+	protected String email;
+	protected String language;
+	
+}
