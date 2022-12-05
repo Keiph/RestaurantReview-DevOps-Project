d0ff85f1ab5911dcd20db1980b806fc7bc77ab97
keiph <2100860B@student.tp.edu.sg>
2022-12-05 17:37:06 +0800

    (v1.1) Change file name of RestaurantServlet
    Wrong naming of the file with respect to the one shown in lab sheet. As
    this servlet only deals with "Creating" of a restaurant

--- src/main/java/RegisterRestaurantServlet.java
+++ src/main/java/RegisterRestaurantServlet.java
@@ -11,19 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 
 /**
  * Servlet implementation class RestaurantServlet
  */
 @WebServlet("/RestaurantServlet")
-public class RestaurantServlet extends HttpServlet {
+public class RegisterRestaurantServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public RestaurantServlet() {
+	public RegisterRestaurantServlet() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
