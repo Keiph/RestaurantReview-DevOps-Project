59af632b291d1b780417733d0babcafabf79dfcc
keiph <2100860B@student.tp.edu.sg>
2022-12-06 15:06:26 +0800

    (v1.2) Added Update functionality

--- src/main/java/RestaurantServlet.java
+++ src/main/java/RestaurantServlet.java
@@ -36,7 +36,7 @@ public class RestaurantServlet extends HttpServlet {
 	private static final String SELECT_RESTAURANT_BY_NAME = "select restaurant_name, restaurant_location, restaurant_open_time, restaurant_close_time, restaurant_description, cuisine_category from RestaurantDetails where restaurant_name =?";
 	private static final String SELECT_ALL_RESTAURANTS = "select * from RestaurantDetails ";
 	private static final String DELETE_RESTAURANT_SQL = "delete from RestaurantDetails where restaurantName = ?;";
-	private static final String UPDATE_RESTAURANT_SQL = "update RestaurantDetails set restaurantName = ?,restaurantLocation= ?, openingTime =?,closingTime =?, restaurantDescription =?, restaurantCuisine =? where restaurantName = ?;";
+	private static final String UPDATE_RESTAURANT_SQL = "update RestaurantDetails set restaurant_name = ?,restaurant_location= ?, restaurant_open_time =?,restaurant_close_time =?, restaurant_description =?, cuisine_category =? where restaurant_name= ?;";
 
 	// Implement the getConnection method which facilitates connection to the
 	// database via JDBC
@@ -103,7 +103,7 @@ public class RestaurantServlet extends HttpServlet {
 			throws SQLException, ServletException, IOException {
 		// get parameter passed in the URL
 		String restaurantName = request.getParameter("restaurantName");
-		Restaurant existingRestaurant = new Restaurant("", "", "", "","","");
+		Restaurant existingRestaurant = new Restaurant("", "", "", "", "", "");
 		// Step 1: Establishing a Connection
 		try (Connection connection = getConnection();
 				// Step 2:Create a statement using connection object
@@ -119,7 +119,8 @@ public class RestaurantServlet extends HttpServlet {
 				String closingTime = rs.getString("restaurant_closing_time");
 				String restaurantDescription = rs.getString("restaurant_description");
 				String restaurantCuisine = rs.getString("cuisine_category");
-				existingRestaurant = new Restaurant(restaurantName, restaurantLocation, openingTime, closingTime,restaurantDescription, restaurantCuisine);
+				existingRestaurant = new Restaurant(restaurantName, restaurantLocation, openingTime, closingTime,
+						restaurantDescription, restaurantCuisine);
 			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
@@ -127,6 +128,32 @@ public class RestaurantServlet extends HttpServlet {
 		// Step 5: Set existingUser to request and serve up the userEdit form
 		request.setAttribute("restaurant", existingRestaurant);
 		request.getRequestDispatcher("/restaurantEdit.jsp").forward(request, response);
+	}
+
+	// method to update the restaurant table base on the form data
+	private void updateUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		// Step 1: Retrieve value from the request
+		String oriRestaurantName = request.getParameter("oriRestaurantName");
+		String restaurantName = request.getParameter("restaurantName");
+		String restaurantLocation = request.getParameter("restaurantLocation");
+		String openingTime = request.getParameter("openingTime");
+		String closingTime = request.getParameter("closingTime");
+		String restaurantDescription = request.getParameter("restaurantDescription");
+		String restaurantCuisine = request.getParameter("restaurantCuisine");
+		// Step 2: Attempt connection with database and execute update user SQL query
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement(UPDATE_RESTAURANT_SQL);) {
+			statement.setString(1, restaurantName);
+			statement.setString(2, restaurantLocation);
+			statement.setString(3, openingTime);
+			statement.setString(4, closingTime);
+			statement.setString(5, restaurantDescription);
+			statement.setString(6, restaurantCuisine);
+			statement.setString(7, oriRestaurantName);
+			int i = statement.executeUpdate();
+		}
+		// Step 3: redirect back to UserServlet
+		response.sendRedirect("http://localhost:8090/RestaurantReview/RestaurantServlet/dashboard");
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
