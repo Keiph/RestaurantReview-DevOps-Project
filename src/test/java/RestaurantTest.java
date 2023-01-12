d68e3b1c1b98f424fffcb2abf13eff63dbb7456b
keiph <2100860B@student.tp.edu.sg>
2023-01-13 00:41:05 +0800

    - Added Restaurant Test File for unit testing

---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,82 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RestaurantTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testRestaurant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetRestaurantName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetRestaurantName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetRestaurantLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetRestaurantLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetOpeningTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetOpeningTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetClosingTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetClosingTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetRestaurantDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetRestaurantDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetRestaurantCuisine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetRestaurantCuisine() {
+		fail("Not yet implemented");
+	}
+
+}
